ca44f9a856a948b92ed8bfd798b461fe6af8f9d7
tiffcodes <tiffnogueira@gmail.com>
2018-02-22 17:29:10 -0500

    initial commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,34 @@
+const drinks = {
+	icedcoffee: [
+		{
+			title: 'Early Bird',
+			price: '$$'
+		},
+		{	
+			title: 'Dark Horse',
+			price: '$'
+		},
+		{
+			title: 'Starbucks',
+			price: '$$'
+		}
+	],
+	beer: [
+		{
+			title: 'Java House',
+			price: '$'
+		},
+		{
+			title: 'Horseshoe Tavern',
+			price: '$$'
+		},
+		{
+			title: 'Tequila Bookworm',
+			price: '$'
+		}
+	]
+};
+$(function() {
+	//Our code goes in here
+});
+
